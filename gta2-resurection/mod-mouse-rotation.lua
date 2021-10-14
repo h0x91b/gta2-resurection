dfaac449b7d94cee9817ba76ea48662f10832330
WIN-9N4G5P4MD1U\user <h0x91b@gmail.com>
2021-10-14 22:39:53 +0300

    [+] Updated ghidra project

--- gta2-resurection/mod-mouse-rotation.lua
+++ gta2-resurection/mod-mouse-rotation.lua
@@ -93,8 +93,6 @@ function Mod.setKeyState( keys, api )
 	if api.IsRightMouseDown then
 		if bitand(keys, 0x80) == 0 then
 			if getSetting("Fly on right click") then
-				ped.obj.player.forward = 1
-				ped.obj.field_0x258 = 0x23
 				PedJumpFly(ped.obj, 0)
 			else
 				keys = keys + 0x80
